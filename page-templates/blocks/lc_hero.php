17e041b33107fcdabdee0942df3df10c7577ca18
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-02 16:02:36 +0000

    Update lc_hero.php

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -1,7 +1,9 @@
 <?php
 $bg = wp_get_attachment_image_url(get_field('background'), 'full');
+$classes = $block['className'] ?: 'pb-5';
 ?>
-<section class="hero" style="background-image: url(<?=$bg?>)">
+<section class="hero <?=$classes?>"
+    style="background-image: url(<?=$bg?>)">
     <div class="hero__inner px-4">
         <h1 class="h1 my-auto d-inline">
             <?=get_field('title')?>
