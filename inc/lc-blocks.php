6c39dc0949e0f19a35bfc31fd41751b4168fdfa9
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-03 17:05:11 +0000

    text/map block

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -40,6 +40,15 @@ function acf_blocks()
             'supports' => array('mode' => false),
         ));
         acf_register_block_type(array(
+            'name'				=> 'lc_text_map',
+            'title'				=> __('LC Text/Map'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/lc_text_map.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
+        acf_register_block_type(array(
             'name'				=> 'lc_full_text',
             'title'				=> __('LC Full Text'),
             'category'			=> 'layout',
---
+++ page-templates/blocks/lc_text_map.php
@@ -0,0 +1,62 @@
+<?php
+$txtcol = get_field('order') == 'text' ? 'order-2 order-md-1' : 'order-2 order-md-2';
+$imgcol = get_field('order') == 'text' ? 'order-1 order-md-2' : 'order-1 order-md-1';
+
+$txtanim = get_field('order') == 'text' ? 'fade-right' : 'fade-left';
+$imganim = get_field('order') == 'text' ? 'fade-left' : 'fade-right';
+
+$txtanim = 'fade';
+$imganim = 'fade';
+
+$classes = $block['className'] ?? 'py-5';
+
+?>
+<section class="feature <?=$classes?>">
+    <div class="container-xl">
+        <?php
+        if (get_field('title') ?? null) {
+            ?>
+        <div class="h2 text-center d-md-none" data-aos="fade">
+            <?=get_field('title')?>
+        </div>
+        <?php
+        }
+?>
+        <div class="row g-4">
+            <div class="col-md-6 <?=$txtcol?> d-flex flex-column justify-content-center"
+                data-aos="<?=$txtanim?>">
+                <?php
+        if (get_field('title') ?? null) {
+            ?>
+                <h2 class="d-none d-md-block h2">
+                    <?=get_field('title')?>
+                </h2>
+                <?php
+        }
+?>
+                <div><?=get_field('content')?>
+                </div>
+                <?php
+if (get_field('link') ?? null) {
+    $l = get_field('link');
+    ?>
+                <a href="<?=$l['url']?>"
+                    target="<?=$l['target']?>"
+                    class="mt-4 btn btn-red text-center align-self-center align-self-md-start"><?=$l['title']?></a>
+                <?php
+}
+?>
+            </div>
+            <div class="col-md-6 <?=$imgcol?> d-flex align-items-center"
+                data-aos="<?=$imganim?>">
+                <?php
+$map_url = get_field('map_url');
+
+?>
+                <iframe src="<?=$map_url?>" width="600" height="450"
+                    style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
