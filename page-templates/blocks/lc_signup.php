72d7819e871b3133653286e95020bf48a4f717b4
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-04 15:07:11 +0000

    signup

--- page-templates/blocks/lc_signup.php
+++ page-templates/blocks/lc_signup.php
@@ -1,7 +1,7 @@
 <section class="signup py-4">
     <div class="container-xl">
         <h2 class="h2 text-center mb-1">Tickets on sale soon</h2>
-        <div class="text-center">Enter your email address to be the first to know.</div>
+        <div class="text-center mb-3">Enter your email address to be the first to know.</div>
         <div class="signup_form">
             <?=do_shortcode('[contact-form-7 id="' . get_field('form_id') . '"]')?>
         </div>
